30765e6b6f5de5e91266a3353e3bb715b9bee57d
OhmarCB <104176599+OhmarCB@users.noreply.github.com>
2022-08-18 23:34:15 -0500

    Add files via upload

---
+++ ET-VICTORCHUMACERO-Eureca-Server/src/test/java/EFVICTORCHUMACEROEurecaServer/EfVictorChumaceroEurecaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package EFVICTORCHUMACEROEurecaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfVictorChumaceroEurecaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
